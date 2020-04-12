6d5147660b71f9ef36571a536bec9ec55992c061
Ram in <raminhafezi@gmail.com>
2020-04-12 11:35:32 +1000

    add eventListener to the backdrop and button

--- Chapter 9 : Working with JavaScript & CSS/javascript/shared.js
+++ Chapter 9 : Working with JavaScript & CSS/javascript/shared.js
@@ -2,14 +2,39 @@
 
 var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
-// console.dir(backdrop)
-// backdrop.style.display = 'block';
+var modalNoButton = document.querySelector('.modal__action--negative');
+var modalYesButton = document.querySelector(".modal__action");
 var selectPlanButton = document.querySelectorAll('.plan button');
-// console.log(selectPlanBotton);
 
-for (var i = 0; i < selectPlanBotton.length; i++) {
+// var mobileNavDisplay = document.querySelector(".main-header__brand img");
+// var mobileNav = document.querySelector('.mobile-nav');
+// console.log(mobileNavDisplay)
+
+for (var i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', function () {
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     });
 };
+
+backdrop.addEventListener("click", closeModal);
+modalNoButton.addEventListener('click', closeModal);
+modalYesButton.addEventListener('click', continueToHostingPage);
+// mobileNavDisplay.addEventListener('click', function () {
+//     backdrop.style.display = "block";
+//     mobileNav.style.display = "block";
+// });
+
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+    // mobileNav.style.display = "none";
+};
+
+function continueToHostingPage() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+    document.location.href = "start-hosting.html";
+};
+
+
